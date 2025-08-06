ea3296241cd08d03c3fdc5fe1d1bdcd2dac79383
Namdevv <namtran34311@gmail.com>
2025-08-06 19:50:49 +0700

    fix: use  for Render compatibility

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-python -m gunicorn --bind 0.0.0.0:8000 --workers 3 APIT.wsgi:application
+gunicorn --bind 0.0.0.0:$PORT --workers 3 APIT.wsgi:application
